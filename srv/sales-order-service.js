08b92ae8c4da4dacaf8ce80c2eb94bd408239063
Ching Hong Chong <chinghong.chong@rizing.com>
2026-09-04 07:42:08 +0000

    update

--- srv/sales-order-service.js
+++ srv/sales-order-service.js
@@ -7,8 +7,9 @@ export default class SalesOrderService extends cds.ApplicationService {
     const remote = await cds.connect.to('SalesOrderAPI')
 
     this.on('get_sales_order', async () => {
-      const order = await remote.send({ method: 'GET', path: '/' })
-      return JSON.stringify(order);      
+      const order = await remote.send({ method: 'GET', path: '/http/ch/s4/saml2bearer/salesorder' })
+      //return JSON.stringify(order);      
+      return order;
     })
 
     return super.init()
